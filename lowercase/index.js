1a86e4645a78e36130ddf14a351760d058413fc5
dora727 <dora727@users.noreply.github.com>
2024-06-26 13:36:22 +0000

    deploy: a14c493e65cd5ff0eb89c0b3a6bc1178b24eb37a

---
+++ lowercase/index.js
@@ -0,0 +1,1 @@
+(function(t,e,o){"use strict";const a=o.before("render",e.ReactNative.Text,function(r){let[n]=r;const l={...e.ReactNative.StyleSheet.flatten(n.style)??{},textTransform:"lowercase"};n.style=l});function c(){a()}return t.onUnload=c,t})({},vendetta.metro.common,vendetta.patcher);
